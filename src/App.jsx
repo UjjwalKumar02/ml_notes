02bc9a01c07894407f7795b6e4993ee6fc83f77c
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2025-02-18 20:29:45 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,47 @@
 import { useState } from 'react'
 import Navbar from './components/header/Navbar'
 import Footer from './components/footer/Footer'
-import SLR from './components/pages/SLR'
+import SLR from './components/pages/SLR.jsx'
 import MLR from './components/pages/MLR'
-import Index from './components/pages/Index'
 
 function App() {
-  const Index = () => <Index />
-  const SLR = () => <SLR />
-  const MLR = () => <MLR />
+  const Slr = () => <SLR />
+  const Mlr = () => <MLR />
 
   const [selectedComp, setSelectedComp] = useState(null)
-  const [isVisible, setIsVisible] = useState(true)
+  const [showList, setShowList] = useState(false)
 
   const components = [
-    {id: 0, label: "Index", component: Index},
-    {id: 1, label: "Simple linear regression", component: SLR},
-    {id: 2, label: "Multiple linear regression", component: MLR}
+    {id: 1, label: "Simple linear regression", component: Slr},
+    {id: 2, label: "Multiple linear regression", component: Mlr},
+    {id: 3, label: "Updating soon...", component: Slr}
   ]
 
   const handleClick = (id) => {
     setSelectedComp(id)
+    showList(false)
   }
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible)
-  }
-  const handleClickContent = () => {
-    setSelectedComp(0)
+
+  const toggleList = () => {
+    setShowList(!showList)
+    setSelectedComp(null)
   }
 
   return (
     <div className='bg-[#ffffff] md:text-[1.25rem] text-[1rem]'>
-     <Navbar />
+     <Navbar toggleList={toggleList}/>
      
      
-      <div className='mt-40 flex flex-col gap-4'>
-       {components.map(({id, label}) => (
-        <button 
-        key={id}
-        onClick={() => handleClick(id)}
-        className='cursor-pointer'>{label}</button>
-       ))}
-      </div>
+      {selectedComp === null && (
+        <div className='mt-40 flex flex-col gap-4 h-dvh'>
+        {components.map(({id, label}) => (
+         <button 
+         key={id}
+         onClick={() => handleClick(id)}
+         className=''>{label}</button>
+        ))}
+       </div>
+      )}
      
      
      <div>
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Navbar = () => {
+const Navbar = ({toggleList}) => {
   return (
     <nav className='w-[100%] flex justify-between items-center px-7 md:py-9 py-7 rounded-lg text-black fixed top-0 bg-[#ffffff] bg-opacity-80 z-1000'>
       <p className='md:text-[1.5rem] text-[1.25rem] font-bold'>
        ML_Notes
       </p>
       <div className='flex gap-4 items-center'>
-        <a href="" className='font-semibold bg-black text-white border border-gray-800 rounded-lg px-3 py-1 hover:bg-gray-900'>Table of contents</a>
+        <button onClick={toggleList} className='font-semibold bg-black text-white border border-gray-800 rounded-lg px-3 py-1 hover:bg-gray-900'>Table of contents</button>
         <a href="" className='font-semibold md:block hidden'>About</a>
         <a href="" className='font-semibold md:block hidden'>Contact</a>
       </div>
