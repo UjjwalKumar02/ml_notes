8ad7d1d51d04f6ed38141ebc77ff8434e5ec3460
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2025-02-20 23:06:01 +0530

    added implementations

--- src/components/implement-pages/SLRImplementation.jsx
+++ src/components/implement-pages/SLRImplementation.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const SLRImplementation = ({handleImplementationClick}) => {
   return (
     <section 
-    className='mt-32 flex flex-col items-center'
+    className='md:mt-32 mt-26 flex flex-col items-center'
     >
       <button 
-      className='bg-black text-white px-3 py-1 rounded-3xl font-semibold cursor-pointer hover:bg-gray-700 border border-gray-800 mb-2' 
+      className='bg-black text-white px-3 py-1 rounded-lg font-semibold cursor-pointer hover:bg-gray-700 border border-gray-800 md:w-[65%] w-[95%]' 
       onClick={handleImplementationClick}
       >
         Hide
@@ -16,7 +16,7 @@ const SLRImplementation = ({handleImplementationClick}) => {
       src="https://nbviewer.org/github/UjjwalKumar02/SLR/blob/main/01_simpleLinearReg.ipynb" 
       frameborder="0"
       height="600px"
-      className='md:w-[65%] w-[95%] mx-auto border border-gray-300 md:p-6 py-3 rounded-lg shadow-lg' 
+      className='md:w-[65%] w-[100%] mx-auto border border-gray-300 md:p-6 py-3 px-1 rounded-lg shadow-lg' 
       />
     </section>
   )
